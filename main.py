943da98a46eefd54e3c1651efd314fa5727b67d2
Bea Stollnitz <bea@stollnitz.com>
2020-02-29 19:34:01 -0800

    Project setup.

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Dict, List, Tuple
+import numpy as np
+
+S3_URL = 'https://bea-portfolio.s3-us-west-2.amazonaws.com/activity-classification/'
+
+DATA_FOLDER = 'data'
+SPECTROGRAMS_FOLDER = 'spectrograms'
+PLOTS_FOLDER = 'plots'
+
+
+def main() -> None:
+    """Main program.
+    """
+    print('Activity classification.')
+
+
+if __name__ == '__main__':
+    main()
